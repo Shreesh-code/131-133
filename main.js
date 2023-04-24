a1fecd1b4893ae6094f7c6f9137a9c9a9413a8ac
Shreesh-code <83659368+Shreesh-code@users.noreply.github.com>
2023-04-24 16:31:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function living()
+{
+    window.location = "living.html";
+}
+
+function balcony()
+{
+    window.location = "balcony.html";
+}
+function bottles()
+{
+    window.location = "bottles.html";
+}
+function helpMe()
+{
+    window.location = "help.html";
+}
